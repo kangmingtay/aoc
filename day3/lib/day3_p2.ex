bcb0bcc389e3bef0fc04aaeea3fdf60e9da0c183
kangmingtay <kang.ming1996@gmail.com>
2021-12-06 15:03:33 +0800

    day3 part2 refactor

--- day3/lib/day3_p2.ex
+++ day3/lib/day3_p2.ex
@@ -6,40 +6,34 @@ defmodule Day3.Part2 do
     |> Enum.map(&(String.to_charlist(&1) |> List.to_tuple))
   end
 
-  def get_majority_in_col(rows, pos, num_rows, type) do
-    half = div(num_rows, 2)
-
-    zeros =
-      rows 
-      |> Enum.count_until(&(elem(&1, pos) == ?0), half+1)
-    
-    get_majority_by_type(zeros, half, type)
+  def o2(rows) do
+    get_rating(rows, 0, fn zeros, ones -> 
+      if zeros > ones, do: ?0, else: ?1
+    end)
+    |> Tuple.to_list 
+    |> List.to_integer(2)
   end
 
-  def get_majority_by_type(zeros, half, _type = :oxygen) when zeros > half, do: ?0
-  def get_majority_by_type(zeros, half, _type = :oxygen) when zeros <= half, do: ?1
-  def get_majority_by_type(zeros, half, _type = :co2) when zeros <= half, do: ?0
-  def get_majority_by_type(zeros, half, _type = :co2) when zeros > half, do: ?1
-
-  def handle_tiebreaker(_val = :tiebreaker, _type = :oxygen), do: 1
-  def handle_tiebreaker(_val = :tiebreaker, _type = :co2), do: 0
-
-  def get_rating([x], _pos, _type), do: x
-  def get_rating(rows, pos, type) do
-    num = get_majority_in_col(rows, pos, length(rows), type)
-    rows 
-      |> Enum.filter(&(elem(&1, pos) == num))
-      |> get_rating(pos+1, type)
+  def co2(rows) do
+    get_rating(rows, 0, fn zeros, ones ->
+      if zeros > ones, do: ?1, else: ?0
+    end)
+    |> Tuple.to_list 
+    |> List.to_integer(2)
   end
 
+  defp get_rating([x], _pos, _fun), do: x
+  defp get_rating(rows, pos, fun) do
+    zeros = rows |> Enum.count(&(elem(&1, pos) == ?0))
+    ones = length(rows) - zeros
+    num = fun.(zeros, ones)
+    filtered_rows = Enum.filter(rows, &(elem(&1, pos) == num))
+    get_rating(filtered_rows, pos+1, fun)
+  end
+ 
   def sol(path) do
-    rows = 
-      path
-      |> parse_input()
-
-    oxygen_rating = rows |> get_rating(0, :oxygen) |> Tuple.to_list |> List.to_integer(2)
-    co2_rating = rows |> get_rating(0, :co2) |> Tuple.to_list |> List.to_integer(2)
-    oxygen_rating * co2_rating
+    rows = path |> parse_input()
+    o2(rows) * co2(rows)
   end
 
 end
